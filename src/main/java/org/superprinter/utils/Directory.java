a78518c4648eeefa7baf39ca02e750b6524dbb10
Alejacks <ald@MBP13.local>
2026-01-29 16:29:27 +0100

    feat: Added Directory file with final variables with paths to be able to set up the program with ease

---
+++ src/main/java/org/superprinter/utils/Directory.java
@@ -0,0 +1,9 @@
+package org.superprinter.utils;
+
+public class Directory {
+
+    public static final String LIBRARY_BOOKS_PATH = "./src/main/java/org/superprinter/library";
+    public static final String OFFICE_DOCUMENTS_PATH = "./src/main/java/org/superprinter/office";
+    public static final String STATIONERS_DOCUMENTS_PATH = "./src/main/java/org/superprinter/stationers_room";
+
+}
