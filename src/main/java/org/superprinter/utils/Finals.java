81974a15559e156fb6ddf2fea681666061635c93
Alejacks <ald@MBP13.local>
2026-01-30 19:34:03 +0100

    feat: Added finals for employees count and printers count

--- src/main/java/org/superprinter/utils/Finals.java
+++ src/main/java/org/superprinter/utils/Finals.java
@@ -3,4 +3,9 @@ package org.superprinter.utils;
 public class Finals {
 
     public static final int PAGE_SIZE = 400;
+
+    public static final int EMPLOYEES_COUNT = 5;
+    public static final int BLACK_AND_WHITE_PRINTERS_COUNT = 3;
+    public static final int COLOR_PRINTERS_COUNT = 2;
+
 }
