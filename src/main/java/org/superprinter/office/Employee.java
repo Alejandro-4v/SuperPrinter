012da2c045491f7af8e2ded11bc96b3297aa5a64
Alejacks <ald@MBP13.local>
2026-01-30 19:44:20 +0100

    feat: Office updated with a main method and methods to start some services. Employee now implements Runnable and has all properties defined with no methods yet

--- src/main/java/org/superprinter/office/Employee.java
+++ src/main/java/org/superprinter/office/Employee.java
@@ -1,4 +1,49 @@
 package org.superprinter.office;
 
-public class Employee {
+import org.superprinter.library.Librarian;
+import org.superprinter.server_room.Server;
+import org.superprinter.utils.DocumentType;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Employee implements Runnable {
+
+    private String name;
+    private int threadId;
+
+    private final AtomicInteger WRITTEN_DOCUMENTS = new AtomicInteger();
+
+    private static final Random RND = new Random();
+
+    private final Server COMMON_SERVER;
+    private final Librarian LIBRARIAN;
+
+    public Employee(String name, Server COMMON_SERVER, Librarian LIBRARIAN) {
+        this.name = name;
+        this.COMMON_SERVER = COMMON_SERVER;
+        this.LIBRARIAN = LIBRARIAN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getThreadId() {
+        return threadId;
+    }
+
+    public void setThreadId(int threadId) {
+        this.threadId = threadId;
+    }
+
+    public void run() {
+
+    };
+
+
 }
--- src/main/java/org/superprinter/office/Office.java
+++ src/main/java/org/superprinter/office/Office.java
@@ -1,4 +1,53 @@
 package org.superprinter.office;
 
+import org.superprinter.library.Librarian;
+import org.superprinter.server_room.Server;
+import org.superprinter.utils.Directory;
+import org.superprinter.utils.Finals;
+
+import java.util.List;
+import java.util.Random;
+
 public class Office {
+
+    private static final Random RND = new Random();
+
+    private static List<Thread> EMPLOYEES;
+    private static Server MAIN_SERVER;
+    private static Librarian OFFICE_LIBRARIAN;
+
+    public static void main(String[] args) {
+
+        welcomeLibrarian();
+
+        startMainServer();
+
+        welcomeEmployees();
+
+        getEmployeesToWork();
+
+    }
+
+    private static void welcomeEmployees() {
+        for (int i = 0; i < Finals.EMPLOYEES_COUNT; i++) {
+            Employee newEmployee = new Employee("employee_" + i, MAIN_SERVER, OFFICE_LIBRARIAN);
+            Thread newEmployeeThread = new Thread(newEmployee);
+            EMPLOYEES.add(newEmployeeThread);
+        }
+    }
+
+    private static void getEmployeesToWork() {
+        for (Thread employee : EMPLOYEES) {
+            employee.start();
+        }
+    }
+
+    private static void startMainServer() {
+        MAIN_SERVER = new Server();
+    }
+
+    private static void welcomeLibrarian() {
+        OFFICE_LIBRARIAN = new Librarian("Teresa", Directory.LIBRARY_BOOKS_PATH);
+    }
+
 }
